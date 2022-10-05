22674ad0125d93a13c5537ff33742efb6868388e
denis.yudin <rng48322@gmail.com>
2022-10-06 01:29:47 +0300

    Base class finished

---
+++ laba1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Base base1 = new Base();
+            base1.Input();
+            base1.Output();
+        }
+    }
+}
